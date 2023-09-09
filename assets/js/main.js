e3e0476a66447bea624fe7bf6dabcd1beaab5662
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-09 16:26:55 -0300

    [refactoring]code-organization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function convertPokemonToHtml(pokemon){
   return `
@@ -21,14 +19,12 @@ function convertPokemonToHtml(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
+  pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
+    
     for (let i = 0; i < pokemons.length; i++) {
       const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+      listItems.push(convertPokemonToHtml(pokemon))
     }
+    
   })
-  .catch((error) => console.error(error))
