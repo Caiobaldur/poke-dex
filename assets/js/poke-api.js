0cd25576aa4fb3cf3747bf3b411248749554a3be
Caiobaldur <caiobrafachino@hotmail.com>
2023-09-14 12:06:41 -0300

    [refactoring]Details-api--refactoring

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,27 @@
 
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon()
+  pokemon.number = pokeDetail.order
+  pokemon.name = pokeDetail.name
+
+  pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+const [type] = types
+
+  pokemon.types = types
+  pokemon.type = type
+
+  pokemon.photo = pokeDetail.sprites.other.home.front_default
+
+  return pokemon
+
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url)
           .then((response) => response.json())
+          .then(convertPokeApiDetailToPokemon)
 }
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
@@ -13,5 +31,4 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails)
-    .catch((error) => console.error(error))
 }
